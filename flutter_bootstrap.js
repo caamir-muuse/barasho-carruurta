70e273146874636dc2185e1a3cffec6160cf0b7f
caamir-muuse <167996882+caamir-muuse@users.noreply.github.com>
2026-08-23 15:20:59 +0300

    Listen and Memorize modes in the Quran reader

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3196991604" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3858005328" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
