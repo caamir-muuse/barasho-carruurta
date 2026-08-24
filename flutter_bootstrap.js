58bda51dbf34aabd348f820f216902e7070915f5
caamir-muuse <167996882+caamir-muuse@users.noreply.github.com>
2026-08-24 09:35:39 +0300

    Lessons as slides

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3858005328" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "869553527" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
