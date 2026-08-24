60f7d9e047bd1f7f383aeeed806e85fe973b0e08
caamir-muuse <167996882+caamir-muuse@users.noreply.github.com>
2026-08-24 10:26:07 +0300

    Rename to Xarafle

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "869553527" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3050587462" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
